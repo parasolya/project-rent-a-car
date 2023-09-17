480193e070518772f8c3c0c9fb2b90ecbf517a54
parasolya <113554970+parasolya@users.noreply.github.com>
2023-09-17 21:04:19 +0000

    Deploying to gh-pages from @ parasolya/project-rent-a-car@05b35d5a87fe9812ac8f3ecbb249ecb8c149adf7 🚀

---
+++ static/js/794.2701e9c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject_rent_a_car=self.webpackChunkproject_rent_a_car||[]).push([[794],{794:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page"})]})}}}]);
+//# sourceMappingURL=794.2701e9c7.chunk.js.map
