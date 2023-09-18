aaae69545fb7f25b23143e398ea881500004a0b1
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-09-18 22:29:14 +0300

    fix home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,13 @@ import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <>
+    <div className={styles.page}>
       <div className={styles.wrapperTitle}>
         <h1 className={styles.title1}>Миттєва свобода на чотирьох колесах!</h1>
         <h2 className={styles.title2}>Ваш комфорт - наші автомобілі.</h2>
       </div>
       <div className={styles.wrapper}></div>
-    </>
+    </div>
   );
 };
 
